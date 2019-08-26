fce2c7c3ffd6582d848d9347d8483e985218d963
進撃の雯雯老師 <askahu6@gmail.com>
2019-08-26 13:21:09 +0800

    Update VAE.R

--- VAE.R
+++ VAE.R
@@ -1,4 +1,3 @@
-
 rm(list=ls())
 library(keras)
 
@@ -45,22 +44,56 @@ vae <- function(X,intermediate_dim,latent_dim,epsilon_std=1,epochs=50,batch_size
     batch_size = batch_size 
   )
   return(list(score=(encoder%>%predict(X)),
-    encoder=encoder,decoder=generator,vae=vae,history=history))
+              encoder=encoder,decoder=generator,vae=vae,history=history))
 }
 
-#Test
+# QPCA
 
-init <- function(X){
-  X <- (X-min(X))/(max(X)-min(X))
-  X <- array_reshape(X,c(nrow(X),length(X)/nrow(X)))
-  X
+pca <- function(A,rank=0,ifscale=TRUE){
+  if(ifscale){A <- scale(as.matrix(A))[,]}
+  A.svd <- svd(A)
+  if(rank==0|rank==ncol(A)){
+    d <- A.svd$d
+  } else {
+    d <- A.svd$d-A.svd$d[min(rank+1,nrow(A),ncol(A))]
+    d <- d[d > 1e-8]
+  }
+  r <- length(d)
+  prop <- d^2; info <- sum(prop)/sum(A.svd$d^2);prop <- cumsum(prop/sum(prop))
+  d <- diag(d,length(d),length(d))
+  u <- A.svd$u[,1:r,drop=F]
+  v <- A.svd$v[,1:r,drop=F]
+  x <- u%*%sqrt(d)
+  y <- sqrt(d)%*%t(v)
+  z <- x %*% y
+  rlt <- list(rank=r,X=x,Y=y,Z=x%*%y,prop=prop,info=info)
+  return(rlt)
 }
-mnist <- dataset_mnist()
-X <- init(mnist$train$x)
-model <- vae(X,256,10)
+qpca <- function(A,prop=0.99,ifscale=TRUE){
+  x <- pca(A,rank=0,ifscale=TRUE)
+  x <- pca(A,rank=which(x$prop>prop)[1],ifscale=TRUE)
+  x
+}
 
-X.feature <- model$encoder %>% predict(X)
-X.fit <- model$decoder %>% predict(X.feature)
-X.fit2 <- model$vae %>% predict(X)
+# Dummy data
 
-#
+kcol <- 100
+krow <- 1000
+khid <- 3
+X <- lapply(1:khid,function(i){
+  x <- runif(krow,1,2)
+  x <- cbind(x,log(x),x^2,sqrt(x),exp(x))
+  x
+})
+X <- do.call(cbind,X)
+X <- X + rnorm(length(X))/2
+M <- matrix(rnorm(ncol(X)*krow),ncol(X),kcol)
+M[abs(M)<0.25] <- 0
+Z <- X  %*% M
+Z <- Z + rnorm(length(Z))/2
+
+# Test with QPCA
+
+rank <- which((Z.qpca <- qpca(Z))$prop>0.99)[1]
+Z.vae <- vae(Z,intermediate_dim=ncol(Z),latent_dim=rank)
+
