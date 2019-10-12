ef13a5c5c579e2b463e7df243be709872dce62f6
進撃の雯雯老師 <askahu6@gmail.com>
2019-10-12 18:51:32 +0800

    Update vae.py

--- project101/vae.py
+++ project101/vae.py
@@ -1,11 +1,3 @@
-
-###########################################################################
-# vae.py
-# argv = syntax, Zsel, latent_dim, batch_size, epochs
-# argv = ['test.py','250','200','128','10','1']
-# python3 vae.py 25910 200 256 200 0&
-###########################################################################
-
 import csv
 import sys
 import os
@@ -34,25 +26,33 @@ def sampling(args):
     dim = K.int_shape(z_mean)[1]
     epsilon = K.random_normal(shape=(batch, dim))
     return z_mean + K.exp(0.5 * z_log_var) * epsilon
-def vae(X,Y,intermediate_dim=0,latent_dim=0,batch_size=256,epochs=100,verbose=0,validation_split=0.1):
+def vae(X,intermediate_dim=0,latent_dim=0,batch_size=256,epochs=100,verbose=0,validation_split=0.1):
     if intermediate_dim == 0: intermediate_dim = X.shape[1]
     if latent_dim == 0: latent_dim = int(np.floor(intermediate_dim/20))
     if batch_size == 0: batch_size = X.shape[0] 
     input_dim = X.shape[1]
-    output_dim = Y.shape[1]
-    inputs = Input(shape=(input_dim, ), name='encoder_input')
-    x = Dense(intermediate_dim, activation='relu')(inputs)
+    output_dim = X.shape[1]
+    inputs = Input(shape=(input_dim,), name='encoder_input')
+    x = Dense(intermediate_dim, activation='sigmoid')(inputs)
     z_mean = Dense(latent_dim, name='z_mean')(x)
     z_log_var = Dense(latent_dim, name='z_log_var')(x)
     z = Lambda(sampling, output_shape=(latent_dim,), name='z')([z_mean, z_log_var])
     encoder = Model(inputs, [z_mean, z_log_var, z], name='encoder')
     latent_inputs = Input(shape=(latent_dim,), name='z_sampling')
-    x = Dense(intermediate_dim, activation='relu')(latent_inputs)
-    outputs = Dense(output_dim, activation='sigmoid')(x)
+    x = Dense(intermediate_dim, activation='sigmoid')(latent_inputs)
+    outputs = Dense(input_dim, activation='sigmoid')(x)
     decoder = Model(latent_inputs, outputs, name='decoder')
-    vae = Model(inputs, decoder(encoder(inputs)[2]), name='vae_mlp')
-    vae.compile(loss='mse', optimizer='adam')
-    history = vae.fit(Z,Z,
+    outputs = decoder(encoder(inputs)[2])
+    vae = Model(inputs, outputs, name='vae_mlp')
+    reconstruction_loss = mse(inputs, outputs)
+    reconstruction_loss *= input_dim
+    kl_loss = 1 + z_log_var - K.square(z_mean) - K.exp(z_log_var)
+    kl_loss = K.sum(kl_loss, axis=-1)
+    kl_loss *= -0.5
+    vae_loss = K.mean(reconstruction_loss + kl_loss)
+    vae.add_loss(vae_loss)
+    vae.compile(optimizer='adam')
+    history = vae.fit(X,
             batch_size=batch_size,
             epochs=epochs,
             validation_split=validation_split,
@@ -73,7 +73,7 @@ epochs = getarg(argv[4])
 verbose = getarg(argv[5])
 Z = bulk_data[...,range(Zsel)]
 t = datetime.now()
-model, encoder, decoder, history = vae(Z,Z,latent_dim=latent_dim,batch_size=batch_size,epochs=epochs,verbose=verbose)  
+model,encoder,decoder,history = vae(Z,0,0,256,10,1,0.1)
 history = dicts(history.params,history.history)
 history['time'] = (datetime.now()-t).seconds
 history['argv'] = argv
