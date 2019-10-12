292a2a7c817a9a367f95c990fdc9f9b9445da293
進撃の雯雯老師 <askahu6@gmail.com>
2019-10-12 18:50:40 +0800

    Update vae_wdrop.py

--- project101/vae_wdrop.py
+++ project101/vae_wdrop.py
@@ -1,7 +1,7 @@
 import csv
 import sys
 import os
-from keras.layers import Lambda, Input, Dense, Drop
+from keras.layers import Lambda, Input, Dense
 from keras.models import Model
 from keras.datasets import mnist
 from keras.losses import mse, binary_crossentropy
@@ -26,13 +26,13 @@ def sampling(args):
     dim = K.int_shape(z_mean)[1]
     epsilon = K.random_normal(shape=(batch, dim))
     return z_mean + K.exp(0.5 * z_log_var) * epsilon
-def vae(X,Y,intermediate_dim=0,latent_dim=0,batch_size=256,epochs=100,verbose=0,validation_split=0.1):
+def vae(X,intermediate_dim=0,latent_dim=0,batch_size=256,epochs=100,verbose=0,validation_split=0.1):
     if intermediate_dim == 0: intermediate_dim = X.shape[1]
     if latent_dim == 0: latent_dim = int(np.floor(intermediate_dim/20))
-    if batch_size == 0: batch_size = X.shape[0]
+    if batch_size == 0: batch_size = X.shape[0] 
     input_dim = X.shape[1]
-    output_dim = Y.shape[1]
-    inputs = Input(shape=(input_dim, ), name='encoder_input')
+    output_dim = X.shape[1]
+    inputs = Input(shape=(input_dim,), name='encoder_input')
     x = Dropout(0.1)(Dense(intermediate_dim, activation='sigmoid')(inputs))
     z_mean = Dropout(0.1)(Dense(latent_dim, name='z_mean')(x))
     z_log_var = Dropout(0.1)(Dense(latent_dim, name='z_log_var')(x))
@@ -40,11 +40,19 @@ def vae(X,Y,intermediate_dim=0,latent_dim=0,batch_size=256,epochs=100,verbose=0,
     encoder = Model(inputs, [z_mean, z_log_var, z], name='encoder')
     latent_inputs = Input(shape=(latent_dim,), name='z_sampling')
     x = Dropout(0.1)(Dense(intermediate_dim, activation='sigmoid')(latent_inputs))
-    outputs = Dropout(0.1)(Dense(output_dim, activation='sigmoid')(x))
+    outputs = Dropout(0.1)(Dense(input_dim, activation='sigmoid')(x))
     decoder = Model(latent_inputs, outputs, name='decoder')
-    vae = Model(inputs, decoder(encoder(inputs)[2]), name='vae_mlp')
-    vae.compile(loss='mse', optimizer='adam')
-    history = vae.fit(Z,Z,
+    outputs = decoder(encoder(inputs)[2])
+    vae = Model(inputs, outputs, name='vae_mlp')
+    reconstruction_loss = mse(inputs, outputs)
+    reconstruction_loss *= input_dim
+    kl_loss = 1 + z_log_var - K.square(z_mean) - K.exp(z_log_var)
+    kl_loss = K.sum(kl_loss, axis=-1)
+    kl_loss *= -0.5
+    vae_loss = K.mean(reconstruction_loss + kl_loss)
+    vae.add_loss(vae_loss)
+    vae.compile(optimizer='adam')
+    history = vae.fit(X,
             batch_size=batch_size,
             epochs=epochs,
             validation_split=validation_split,
@@ -65,7 +73,7 @@ epochs = getarg(argv[4])
 verbose = getarg(argv[5])
 Z = bulk_data[...,range(Zsel)]
 t = datetime.now()
-model, encoder, decoder, history = vae(Z,Z,latent_dim=latent_dim,batch_size=batch_size,epochs=epochs,verbose=verbose)
+model,encoder,decoder,history = vae(Z,0,0,256,10,1,0.1)
 history = dicts(history.params,history.history)
 history['time'] = (datetime.now()-t).seconds
 history['argv'] = argv
@@ -73,3 +81,4 @@ fo = open('/lustre/wangjc01/huzixin/deconv/log/vae_%s.log' % np.int(np.int(datet
 for k in history:
     fo.write('%s: %s\n' % (k, history[k]))
 fo.close()
+
