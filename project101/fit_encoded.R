5c79f8678f8c7174f7d96cb5ef45ca3fbd931dd8
進撃の雯雯老師 <askahu6@gmail.com>
2019-10-28 21:05:30 +0800

    Update fit_encoded.R

--- project101/fit_encoded.R
+++ project101/fit_encoded.R
@@ -2,7 +2,8 @@ rm(list=ls())
 library(keras)
 library(dplyr)
 source('/Users/wenrurumon/Documents/uthealth/deconv/ae/model.R')
-setwd('/Users/wenrurumon/Documents/uthealth/deconv/rlt')
+# setwd('/Users/wenrurumon/Documents/uthealth/deconv/rlt/adni')
+setwd('/Users/wenrurumon/Documents/uthealth/deconv/rlt/rush')
 models <- dir(pattern='encoded')
 models <- unique(do.call(rbind,strsplit(models,'\\.'))[,1])
 models.data <- lapply(models,function(m){
@@ -10,15 +11,28 @@ models.data <- lapply(models,function(m){
   x <- paste0(m,'.ref_encoded') %>% read.csv
   list(y=y,x=x)
 })
+j <- 0
+
+#DeconvSTF
+
 models.deconv <- lapply(models.data,function(m){
-  rlt.deconv <- deconv(t(m$x),t(m$y))
+  print(paste('####################',j<<-j+1,'####################'))
+  rlt.deconv <- deconv(t(m$x),t(m$y),ifprint=F)
   rlt.fit <- dedeconv((m$x),rlt.deconv$coef,(m$y))
   rlt.fit
 })
-for(i in 1:4){
-  write.csv(models.deconv[[i]],paste0(models[i],'.fit_encoded'),row.names=F)
+for(i in 1:length(models.deconv)){
+  write.csv(models.deconv[[i]],paste0(models[i],'.fit_encoded_stf'),row.names=F)
 }
 
-check <- lapply(models,function(m){
-  read.csv(paste0(m,'.fit_encoded'))
+#DeconvLM
+
+models.deconlm <- lapply(models.data,function(m){
+  print(paste('####################',j<<-j+1,'####################'))
+  rlt.deconv <- deconv.lm(t(m$x),t(m$y),ifprint=F)
+  rlt.fit <- dedeconv((m$x),rlt.deconv,(m$y))
+  rlt.fit
 })
+for(i in 1:length(models.deconlm)){
+  write.csv(models.deconvlm[[i]],paste0(models[i],'.fit_encoded_lm'),row.names=)
+}
