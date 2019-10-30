c08dcda4116ecc575c58b1ed1596e55ba02e6de2
進撃の雯雯老師 <askahu6@gmail.com>
2019-10-30 15:37:38 +0800

    Update reference_alignment.R

--- project101/reference_alignment.R
+++ project101/reference_alignment.R
@@ -1,6 +1,7 @@
 rm(list=ls())
 library(keras)
 library(dplyr)
+library(data.table)
 source('/Users/wenrurumon/Documents/uthealth/deconv/ae/model.R')
 setwd('/Users/wenrurumon/Documents/uthealth/deconv/rlt/encoded_adni')
 
@@ -59,6 +60,14 @@ rlt.adni <- list(
   models.sel,
   models.sellm
 )
+# save(rlt.adni,file='deconv_rlt_adni.rda')
+# 
+# write.csv(t(rlt.adni[[1]][[2]]$deconv[[1]]),'adni_ae_stf.csv')
+# write.csv(t(rlt.adni[[1]][[6]]$deconv[[1]]),'adni_vae_stf.csv')
+# write.csv(t(rlt.adni[[2]][[2]][[1]]),'adni_ae_lm.csv')
+# write.csv(t(rlt.adni[[2]][[6]][[1]]),'adni_vae_lm.csv')
+# write.csv(t(rlt.adni[[3]][[1]]$coef),'adni_sel_stf.csv')
+# write.csv(t(rlt.adni[[4]][[1]]),'adni_sel_lm.csv')
 
 ##################################################################
 ##################################################################
@@ -124,5 +133,35 @@ rlt.rush <- list(
   models.sel,
   models.sellm
 )
+# save(rlt.rush,file='deconv_rlt_rush.rda')
+# 
+# write.csv(t(rlt.rush[[1]][[1]]$deconv[[1]]),'rush_ae_stf.csv')
+# write.csv(t(rlt.rush[[1]][[3]]$deconv[[1]]),'rush_vae_stf.csv')
+# write.csv(t(rlt.rush[[2]][[1]][[1]]),'rush_ae_lm.csv')
+# write.csv(t(rlt.rush[[2]][[3]][[1]]),'rush_vae_lm.csv')
+# write.csv(t(rlt.rush[[3]][[1]]$coef),'rush_sel_stf.csv')
+# write.csv(t(rlt.rush[[4]][[1]]),'rush_sel_lm.csv')
 
+##################################################################
+##################################################################
 
+rm(list=ls())
+setwd('/Users/wenrurumon/Documents/uthealth/deconv/rlt')
+for (i in dir(pattern='rda')) {print(i); load(i)}
+ls()
+adni1 <- rlt.adni[[1]]$ae_16055_1024_250_256_4000_2$deconv$coef
+adni2 <- rlt.adni[[2]]$ae_16055_1024_250_256_4000_2$deconv
+adni3 <- rlt.adni[[1]]$ae_16055_1024_200_256_4000_2$deconv$coef
+adni4 <- rlt.adni[[1]]$vae_16055_1024_250_256_4000_2$deconv$coef
+
+head(adni1[,1:10])
+head(adni3[,1:10])
+head(adni4[,1:10])
+
+rush1 <- rlt.rush[[1]]$ae_25910_1024_250_256_4000_2$deconv$coef
+rush2 <- rlt.rush[[1]]$ae_25910_1024_400_256_4000_2$deconv$coef
+rush3 <- rlt.rush[[1]]$vae_25910_1024_250_256_4000_2$deconv$coef
+
+head(rush1[,1:5])
+head(rush2[,1:5])
+head(rush3[,1:5])
